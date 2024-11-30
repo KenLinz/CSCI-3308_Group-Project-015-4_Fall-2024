a38944cf61eb1627725670ed8b41745b161a46f6
Melanie <melanie.xinyi.porter@gmail.com>
2024-11-29 22:28:55 -0800

    test

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -27,8 +27,8 @@ const hbs = handlebars.create({
 
 // database configuration
 const dbConfig = {
-    //host: process.env.HOST, // the database server ||| RENDER HOST
-    host: 'db', // ||| LOCAL HOST
+    host: process.env.HOST, // the database server ||| RENDER HOST
+    // host: 'db', // ||| LOCAL HOST
     port: 5432, // the database port
     database: process.env.POSTGRES_DB, // the database name
     user: process.env.POSTGRES_USER, // the user account to connect with
